f79ccc80de6be7dbb9a3c4e08d1fc6fb64a23e31
Clauber Rodrigues <clauberrodrigues8@gmail.com>
2022-02-09 23:06:30 -0300

    Api-rest

---
+++ Domain/Helpers/EUnitOfPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace api_rest.Domain.Helpers
+{
+    public enum EUnitOfBook : byte
+    {
+        [Description("Fantasia")]
+        fantasia = 1,
+
+        [Description("Romance")]
+        romance = 2,
+
+        [Description("Suspense")]
+        suspense = 3,
+
+    }
+
+}
---
+++ Mapping/ModelToRescueProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using api_rest.Resources;
+using api_rest.Extensions;
+using api_rest.Domain.Models;
+
+namespace api_rest.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Category, CategoryResource>();
+
+            CreateMap<User, UserResource>();
+
+            CreateMap<Product, ProductResource>()
+                .ForMember(src => src.UnitOfBook, opt => opt.MapFrom(src => src.UnitOfBook.ToDescriptionString()));
+        }
+    }
+}
